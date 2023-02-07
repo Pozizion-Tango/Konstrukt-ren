9ae3351246107fba1d43358b16f5df1acc21a2d3
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2023-02-07 14:09:33 +0100

    Add project files.

---
+++ Konstruktøren/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+
+namespace Konstruktøren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool newrobot = true;// a true statment, used in the code later to help if the user wants to close the program
+            Random rnd = new Random();//used for 1/2 chances for the robot to have either a true or false statment
+            while (newrobot)
+            {
+                newrobot = true;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("==============================================");
+                Console.WriteLine("===        ======          ====          =====");
+                Console.WriteLine("=   ======   ===   ======   ===  =======  ====");
+                Console.WriteLine("=   ======   ===  ========  ===  =======  ====");
+                Console.WriteLine("=   ======   ===  ========  ===         ======");
+                Console.WriteLine("=          =====  ========  ===         ======");
+                Console.WriteLine("=   =====   ====  ========  ===  =======  ====");
+                Console.WriteLine("=   ======   ===   ======   ===  =======  ====");
+                Console.WriteLine("=   =======   ===          ====          =====");
+                Console.WriteLine("==============================================");
+                Console.WriteLine("==============================================");
+                Console.WriteLine("======         ============           ========");
+                Console.WriteLine("=====   ==================   =======   =======");
+                Console.WriteLine("====   ===================  =========  =======");
+                Console.WriteLine("====  ====================  =========  =======");
+                Console.WriteLine("====  ====================  =========  =======");
+                Console.WriteLine("====   ===================  =========  =======");
+                Console.WriteLine("=====   ==================   =======   =======");
+                Console.WriteLine("======         ============           ========");
+                Console.WriteLine("==============================================\r\n\r\n");
+                Console.ResetColor();
+                Console.WriteLine("1: Generate Robot Type (default)\r\n2: Generate Robot Type (Cleaner)\r\n0: Exit");
+                string choice = Console.ReadLine();//above to user gets told what they can select. And Choice is their selected number.
+                bool colour = false;//Default setting
+                string chip = "RX54667";//Default setting
+                bool wifi = false;//Default setting
+                int batterycapacity = 255;//Default setting
+                if (rnd.Next(0, 2) == 0)//Chancing if the robot will have Wifi
+                    wifi = true;
+                
+                if (rnd.Next(0, 2) == 0)
+                {
+                    colour = true;
+                    batterycapacity = 0;
+                }
+                double soapcapacity = 2.3;//Default setting
+                bool small = false;//Default setting
+                int wheels = 0;//Default setting
+                if (rnd.Next(0, 2) == 0)//Chanching if the Robot will be Small
+                    small = true;
+                if (!small)//Chanching if it got wheels if the robot is not small, and how many wheels.
+                    wheels = rnd.Next(0, 5);
+                if (rnd.Next(0, 2) == 0)//Using chance to choose it's chip
+                    chip = "QT8339";
+                if (choice == "1")//If choice is 1, it will have the "Default setting"
+                {
+                    Robot robot1 = new Robot(colour, batterycapacity, chip, wifi, small, wheels);
+                    robot1.Info();
+                }
+                else if (choice == "2")//If choice is 2, it will have the "Cleaner setting"
+                {
+                    Robot robot1 = new Robot(colour, chip, wifi, soapcapacity, small, wheels);
+                    robot1.Info();
+                }
+                else if (choice == "0")//if choice is 0 it will take "newrobot" and turn it false to stop the program from repeating
+                {
+                    newrobot = false;
+                }
+                else//if the user miss spells it will warn the user and restart
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Invalid, press any key to continue");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Konstruktøren/Robot.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktøren
+{
+    internal class Robot
+    {
+        private bool coloured;
+        private string chip;
+        private bool wifi;
+        private int batterycapacity;
+        private double soapcapacity;
+        private bool small;
+        private int wheels;
+        public bool Coloured
+        {
+            get { return coloured; }
+            set { coloured = value; }
+        }
+        public string Chip
+        {
+            get { return chip; }
+            set { chip = value; }
+        }
+        public bool Wifi
+        {
+            get { return wifi; }
+            set { wifi = value; }
+        }
+        public int Batterycapacity
+        {
+            get { return batterycapacity; }
+            set { batterycapacity = value; }
+        }
+        public double Soapcapacity
+        {
+            get { return soapcapacity; }
+            set { soapcapacity = value; }
+        }
+        public bool Small
+        {
+            get { return small; }
+            set { small = value; }
+        }
+        public int Wheels
+        {
+            get { return wheels; }
+            set { wheels = value; }
+        }
+        public Robot(bool coloured, string chip, bool wifi, bool small, int wheels)//Default Robot propities
+        {
+            this.Coloured = coloured;
+            this.Chip = chip;
+            this.Wifi = wifi;
+            this.Small = small;
+            this.Wheels = wheels;
+        }
+        public Robot(bool coloured, int batterycapacity, string chip, bool wifi, bool small, int wheels) : this(coloured, chip, wifi, small, wheels)//robot setting for default
+        {
+            this.Batterycapacity = batterycapacity;
+        }
+        public Robot(bool coloured, string chip, bool wifi, double soapcapacity, bool small, int wheels) : this(coloured, chip, wifi, small, wheels)//robot settings for Cleaner
+        {
+      
+            this.Soapcapacity = soapcapacity;
+        }
+        public void Info()//this will show the info on robots
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Robot Info: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Coloered: "+ this.Coloured);
+            Console.WriteLine("Chip: " + this.Chip);
+            Console.WriteLine("Wifi: " + this.Wifi);
+            if (this.Batterycapacity > 0)//if there is zero on the battery, it will be removed.
+                Console.WriteLine("Battery: " + this.Batterycapacity);
+            if (this.Soapcapacity > 0)//if there is zero on the soap, it will be removed.
+                Console.WriteLine("Soap: " + this.Soapcapacity);
+            Console.WriteLine("Small: " + this.Small);
+            if (!this.small)//if there it is small. it will either show to wheels, or it will be removed.
+                Console.WriteLine("Wheels: " + this.Wheels);
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
